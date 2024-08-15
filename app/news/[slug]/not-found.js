872b1a0e470d5917bf6177bc01a599754c75ca65
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2024-08-15 11:24:37 +0400

    creating parallel routing with app router

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,6 @@
+export default function NewsNotFoundPage() {
+    return <div id="error">
+        <h1>Not Found</h1>
+        <p>Unfortunetely we could not find the requested article.</p>
+    </div>
+}
