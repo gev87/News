7e3ebad349c3521bf5b7e388ed9ffcd999f66725
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2024-08-16 15:16:44 +0400

    adding nested paths and handling routing serverside errors

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,57 @@
+import NewsList from "@/components/main-header/news-list";
+import {
+	getNewsForYear,
+	getAvailableNewsYears,
+	getAvailableNewsMonths,
+	getNewsForYearAndMonth,
+} from "@/lib/news";
+import Link from "next/link";
+
+export default function FilteredNewsPage({ params }) {
+	const filter = params.filter || [];
+	const [selectedYear, selectedMonth] = filter;
+
+	let links = getAvailableNewsYears();
+
+	let news = [];
+
+	if (selectedYear) {
+		if (selectedMonth) {
+			news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+			links = [];
+		} else {
+			news = getNewsForYear(selectedYear);
+			links = getAvailableNewsMonths(selectedYear);
+		}
+	}
+
+	let newsContent = <p>No news found for the selected period.</p>;
+
+	if (news.length) {
+		newsContent = <NewsList news={news} />;
+	}
+
+	if (selectedYear && !news.length) {
+		throw new Error("Invalid Filter");
+	}
+
+	return (
+		<>
+			<header id="archive-header">
+				<nav>
+					<ul>
+						{links.map((link) => {
+							const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`;
+							return (
+								<li key={link}>
+									<Link href={href}>{link}</Link>
+								</li>
+							);
+						})}
+					</ul>
+				</nav>
+			</header>
+			{newsContent}
+		</>
+	);
+}
--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/components/main-header/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FilteredNewsPage({ params }) {
-    const newsYear = params.year;
-    const news = getNewsForYear(newsYear);
-    return <NewsList news={news}/>;
-}
